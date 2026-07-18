e34012b47f2bf662cc16558ebf1b32cff24fd761
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-18 14:32:56 +0530

    floyd's and warshal's algorithm to find shortest path

---
+++ Analysis And Design Algorithms/Warshal's and Floyd's Algorithm/Warshal's Algorithm/warshal.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#define INF 999
+#define max 20
+
+
+void warshal(int p[max][max],int n){
+    int i,k,j;
+
+    for( k=1;k<=n;k++){
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n;j++){
+               p[i][j]=p[i][j]||p[i][k] &&p[k][j];
+                }
+            }
+        }
+
+}
+int main(){
+    int i,j,n,c[max][max];
+    printf("enter the number of vertices\n");
+    scanf("%d",&n);
+    printf("\n Enter Graph Data\n");
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            scanf("%d",&c[i][j]);
+        }
+    }
+    warshal(c,n);
+
+    printf("Shortest path Matrix Is\n");
+      for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            printf("%d ",c[i][j]);
+        }
+        printf(" \n");
+    }
+}
