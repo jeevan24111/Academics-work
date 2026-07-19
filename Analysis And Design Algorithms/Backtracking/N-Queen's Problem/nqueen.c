162e1a34ea62633a44dd45ade7722047abff9dd9
jeevan24111 <jeevanbahaddur616@gmail.com>
2026-07-20 05:12:00 +0530

    changes done in backtracking

--- Analysis And Design Algorithms/Backtracking/N-Queen's Problem/nqueen.c
+++ Analysis And Design Algorithms/Backtracking/N-Queen's Problem/nqueen.c
@@ -1,69 +1,73 @@
-// #include<stdio.h>
-// #include<stdlib.h>
-// #define max 10
+#include<stdio.h>
+#include<stdlib.h>
+#define max 10
 
-// int place(int c[],int r){
-//     int i;
-//     for(i=0;i<r;i++){
-//         if(c[i]==c[r]||(abs(c[i]-c[r]))==abs(i-r)){
-//             return 0;
-//         }
-//     }
-//     return 1;
-// }
+int place(int c[],int r){
+    int i;
+    for(i=0;i<r;i++){
+        if(c[i]==c[r]||(abs(c[i]-c[r]))==abs(i-r)){
+            return 0;
+        }
+    }
+    return 1;
+}
 
-// void display(int c[],int n){
-//     int i,j;
-//     char cb[20][20];
-//     for(i=0;i<n;i++){
-//         for(j=0;j<n;j++){
-//             cb[i][j]='-';
-//         }
-//     }
+void display(int c[],int n){
+    int i,j;
+    char cb[10][10];
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            cb[i][j]='-';
+        }
+    }
 
-//     for(i=0;i<n;i++){
-//         cb[i][c[i]]='Q';
-//     }
+    for(i=0;i<n;i++){
+        cb[i][c[i]]='Q';
+    }
 
-//     for(i=0;i<n;i++){
-//         for(j=0;j<n;j++){
-//             printf("%c",cb[i][j]);
-//         }
-//         printf("\n");
-//     }
-//     printf("\n");
-// }
-// void nQueen(int c[],int r,int n){
-//     int i;
-//     if(r==n){
-//         display(c,n);
-//         return;
-//     }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("%c",cb[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+void nQueen(int c[],int r,int n){
+    int i;
+    if(r==n){
+        display(c,n);
+        return;
+    }
 
-//     for(i=0;i<n;i++){
-//         c[r]=i;
-//         if(place(c,r)){
-//             nQueen(c,r+1,n);
-//         }
-//     }
+    for(i=0;i<n;i++){
+        c[r]=i;
+        if(place(c,r)){
+            nQueen(c,r+1,n);
+        }
+    }
 
-// }
-// int main(){
-//     int n,c[20];
-//     printf("\n============N-Queen==========\n");
-//     printf("ENter the number of queens\n");
-//     scanf("%d",&n);
-//     if(n==2 || n==3){
-//         printf("\n No solution  exits for %d queen\n",n);
-//         return 0;
-//     }
+}
+int main(){
+    int n,c[20];
+    printf("\n============N-Queen==========\n");
+    printf("ENter the number of queens\n");
+    scanf("%d",&n);
+    if(n<=0){
+        printf("invalid input\n");
+        return 1;
+    }
+    if(n==2 || n==3){
+        printf("\n No solution  exits for %d queen\n",n);
+        return 0;
+    }
 
-//     printf("\nplacing %d queen on %d x %dc chessboard\n",n,n,n);
-//     printf("\n====================================================\n");
-//     nQueen(c,0,n);
+    printf("\nplacing %d queen on %d x %dc chessboard\n",n,n,n);
+    printf("\n====================================================\n");
+    nQueen(c,0,n);
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 // #include<stdio.h>
